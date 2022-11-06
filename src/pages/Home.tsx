2f899ec22b42db57dc5a8525a42235fd4cc7d6c5
Mykolas <mykolas.kucinskas2@gmail.com>
2022-11-07 00:06:39 +0200

    feat: added modal component from Material UI

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import EmptyModal from '../components/EmptyModal'
 
 function Home() {
   return (
     <div>
         <span>Hello world</span>
+        <EmptyModal></EmptyModal>
     </div>
   )
 }
