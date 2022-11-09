549608cb4f41b3375f46280d409095c975f83006
Haroldas Bartusevičius <bartuseviciusharoldas@gmail.com>
2022-11-10 01:01:06 +0200

    Added login page and form, currently only accepts these credentials: email:testas@testas password:testas if credentials are correct redirects user to root page, otherwise displays error message.

---
+++ src/features/Login/Login.tsx
@@ -0,0 +1,73 @@
+import React, { useState } from 'react';
+import './Login.css';
+import { NavLink, useNavigate } from 'react-router-dom';
+
+function Login() {
+  const [error, setError] = useState<string | null>(null);
+  const navigate = useNavigate();
+  const [logins, setLogins] = useState({
+    email: '',
+    password: '',
+  });
+
+  // Handle inputs onChange to sync input value with local state
+  const handleInputChange = (event: { target: { name: string; value: string } }) => {
+    setLogins({ ...logins, [event.target.name]: event.target.value });
+  };
+
+  const handleSubmit = (event: { preventDefault: () => void }) => {
+    event.preventDefault();
+    if (logins.email === 'testas@testas' && logins.password === 'testas') {
+      navigate('/');
+    } else {
+      setError('Incorrect username or password');
+    }
+  };
+
+  return (
+    <div>
+      <form className="login-form" onSubmit={handleSubmit}>
+        {error ? <label id="error">{error}</label> : null}
+        <div id="input">
+          <label id="login-label">Email</label>
+          <br />
+          <input
+            id="login-input"
+            type="email"
+            name="email"
+            placeholder="Email@email.com"
+            required
+            value={logins.email}
+            onChange={handleInputChange}
+          />
+        </div>
+        <br />
+        <div id="input">
+          <label id="login-label">Password</label>
+          <br />
+          <input
+            id="login-input"
+            type="password"
+            name="password"
+            placeholder="Password"
+            required
+            value={logins.password}
+            onChange={handleInputChange}
+          />
+        </div>
+        <br />
+        <div id="input">
+          <button id="login-btn" type="submit" className="button">
+            LOGIN
+          </button>
+        </div>
+        <br />
+        <NavLink id="reg" to="/register">
+          Don’t have an account? Click here to create one.
+        </NavLink>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
---
+++ src/features/Login/LoginPage.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Login.css';
+import Login from './Login';
+
+function LoginPage() {
+  return (
+    <div id="login-page">
+      <div id="header">Login</div>
+      <div id="main-section">
+        <Login />
+      </div>
+    </div>
+  );
+}
+
+export default LoginPage;
