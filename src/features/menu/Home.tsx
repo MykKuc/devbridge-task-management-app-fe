d1f23c3ae26b4afaf271e6603d4a62b04185ed04
Titas Dragunas <titas.drag@gmail.com>
2022-11-18 15:09:12 +0200

    added dispatch for testing purpose need redux addon on browser to see created and updated state

--- src/features/menu/Home.tsx
+++ src/features/menu/Home.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import Content from '../../Components/Content';
 import ModalOpenButton from '../../Components/ModalOpenButton';
+import {useDispatch} from "react-redux";
+import {createTask, updateTask, removeTask} from "../../redux/actions/taskActions";
+
+
+
+
 
 function Home() {
+
+  // const dispatch=useDispatch();
+  // dispatch(createTask());
   return (
     <Content name={'Home'} height={'60vh'}>
-      <span>Hello world</span>
+      <span>Hello world </span>
       <ModalOpenButton></ModalOpenButton>
     </Content>
   );
