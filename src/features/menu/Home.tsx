721f892b1b2fa60a5a109e02074a6f20935375d4
Haroldas Bartusevičius <bartuseviciusharoldas@gmail.com>
2022-11-23 17:46:33 +0200

    Added example http request to API

--- src/features/menu/Home.tsx
+++ src/features/menu/Home.tsx
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Content from '../../Components/Content';
 import ModalOpenButton from '../../Components/ModalOpenButton';
 
 function Home() {
+  const [show, setShow] = useState(false);
+  fetch('http://localhost:8080/api/ok').then((response) => {
+    console.log(response);
+    if (response.status == 200) {
+      setShow(true);
+    }
+  });
   return (
     <Content name={'Home'} height={'60vh'}>
       <span>Hello world</span>
       <ModalOpenButton></ModalOpenButton>
+      {show && <label>Veikia API</label>}
     </Content>
   );
 }
