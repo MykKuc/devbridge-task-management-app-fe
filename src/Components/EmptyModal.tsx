eb335a3f9cd842404957473e8dcc6387e10df600
Karolina Ramaskaite <karolina.ramaskaite@devbridge.com>
2022-11-23 13:57:52 +0200

    fixing EmptyModal.css

--- src/Components/EmptyModal.tsx
+++ src/Components/EmptyModal.tsx
@@ -4,7 +4,7 @@ import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
 import { ReactNode, useState } from 'react';
-import './EmptyModal.css';
+import './Modal.css';
 
 interface Props {
   show: boolean;
