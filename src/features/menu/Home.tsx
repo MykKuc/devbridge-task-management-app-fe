a832ade7dc19d811739b8d633b3e7aeabdcf17cc
Tomas Matijoška <tomas.matijoska@gmail.com>
2022-11-29 20:06:53 +0200

    merge conflict fix

--- src/features/menu/Home.tsx
+++ src/features/menu/Home.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import Content from '../../Components/Content';
+import Content from '../../components/Content';
 
 function Home() {
   // const dispatch=useDispatch();
